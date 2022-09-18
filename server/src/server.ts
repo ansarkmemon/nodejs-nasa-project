9780460fbc65eb66f28c96de8637c93c6d28b21f
Ansar Memon <ansar.memon@hotmail.com>
2022-09-18 12:09:14 -0700

    added connection to mongo database

--- server/src/server.ts
+++ server/src/server.ts
@@ -1,12 +1,22 @@
 import http from 'http';
 import { app } from "./app";
 import { loadPlanets } from './models/planets.model';
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+dotenv.config();
 
 const server = http.createServer(app);
 
 const PORT = process.env.PORT || 8000;
 
+const MONGO_URL = process.env.MONGO_URL;
+
+mongoose.connection.once('open', () => console.log('connection ready'));
+mongoose.connection.on('error', (err) => console.error(err))
+
 const startServer = async () => {
+    await mongoose.connect(MONGO_URL);
     await loadPlanets();
 
     server.listen(PORT, () => {
