fdea397fe42d2e76737e533d1a8025f49631777a
Ansar Memon <ansarmemon@Ansars-MacBook-Pro.local>
2022-09-18 08:45:08 -0700

    testing setup

---
+++ server/src/routes/launches/launches.spec.ts
@@ -0,0 +1,20 @@
+import request from "supertest";
+import { app } from "../../app";
+
+describe('Test GET /launches', () => {
+    test('It should respond with 200 success', async () => {
+        const response = await request(app).get('/launches');
+        expect(response.statusCode).toBe(200)
+    })
+});
+
+describe('Test POST /launches', () => {
+    test('It should respond with 200 success', () => {
+        const response = 200;
+        expect(response).toBe(200)
+    });
+
+    test('It should catch missing required properties', () => { })
+
+    test('It should catch invalid dates', () => { })
+});
